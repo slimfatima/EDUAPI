819ec93dc41a9e379c52b919f48d64cc636cf43e
Gabriel Isaac <iamkemical1@gmail.com>
2021-03-17 11:53:13 +0100

    Added HttpRequest on the controller.

--- IEduCare.API/IEduCare.API/Controllers/StudentsController.cs
+++ IEduCare.API/IEduCare.API/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using IEduCare.Shared.Dto;
 using IEduCare.Shared.Interfaces;
+using IEduCare.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,66 @@ namespace IEduCare.API.Controllers
         public ActionResult<List<StudentDto>> Get()
         {
             var students = _studentsManager.GetStudent();
+            if (students is null)
+            {
+                return NotFound();
+            }
             return students;
+        }
+
+        [HttpGet("api/ieducare/student/getstudentbyid")]
+        public ActionResult<StudentDto> GetStudentById(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return BadRequest();
+            }
+            var student = _studentsManager.GetStudentById(id);
+            if(student is null)
+            {
+                return BadRequest();
+            }
+            return student;
+        }
+
+        [HttpPost("api/ieducare/student/createstudent")]
+        public ActionResult<StudentDto> CreateStudent([FromBody] StudentModel model)
+        {
+            if(model is null)
+            {
+                return BadRequest();
+            }
+            var student = _studentsManager.CreateStudent(model);
+            if(student is null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+        [HttpPut("api/ieducare/student/updatestudent")]
+        public ActionResult<StudentDto> UpdateStudent(Guid id, [FromBody] StudentModel model)
+        {
+            if(id == Guid.Empty && model == null)
+            {
+                return BadRequest();
+            }
+            var student = _studentsManager.UpdateStudent(id, model);
+            if(student is null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+        [HttpDelete("api/ieducare/student/deletestudent")]
+        public ActionResult<StudentDto> DeleteStudent(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return BadRequest();
+            }
+            return _studentsManager.DeleteStudent(id);
         }
     }
 }
